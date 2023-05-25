ebdf98f7ce58b31db5adc86801f1b4f454be24f2
Pratima Bharti <54327856+PratimaBharti@users.noreply.github.com>
2023-05-25 23:52:16 +0530

    Update client.cpp

--- client/client.cpp
+++ client/client.cpp
@@ -1,1 +1,17 @@
+#include "myheader.h"
 
+int main(int argc,char *argv[])
+{
+	string ID = argv[1];
+	int sock_fd = registerID(argv[1]);
+
+	//threads created for sending and receiving simultaneously.
+	thread th_send(send_text,sock_fd,ID.c_str());
+	thread th_recv(recv_text,sock_fd);
+
+	th_send.join();
+	th_recv.join();
+	
+	return 0;
+
+}
